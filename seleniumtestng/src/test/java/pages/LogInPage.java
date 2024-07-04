43a8f641ab0872f6d73bf961eb8655e2d2e0533e
criscarlo.dapitanon <criscarlo.dapitanon@legalmatch.com>
2024-07-04 18:24:43 +0800

    Update test case script

--- seleniumtestng/src/test/java/pages/LogInPage.java
+++ seleniumtestng/src/test/java/pages/LogInPage.java
@@ -1,2 +1,33 @@
-package pages;public class LogInPage {
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+
+public class LogInPage {
+
+    public static WebDriver driver;
+    public static void Setup() {
+
+        driver = new ChromeDriver();
+        driver.get("https://demo-store.katalon.com/signin");
+
+    }
+    public static String email_inputField_cssSelector = "input[name='email']";
+    public static  String password_inputField_cssSelector = "input[name='password']";
+    public  static  String signIn_button_cssSelector = "input[value='Sign In']";
+
+    public static void ENTER_EMAIL() {
+        driver.findElement(By.cssSelector(email_inputField_cssSelector)).sendKeys("crisdemo@email.com");
+    }
+
+    public static void ENTER_PASSWORD() {
+        driver.findElement(By.cssSelector(password_inputField_cssSelector)).sendKeys("Testing");
+    }
+
+    public  static void CLICK_SIGNIN_BUTTON() {
+        driver.findElement(By.cssSelector(signIn_button_cssSelector)).click();
+    }
+
 }
--- seleniumtestng/src/test/java/tests/LogInTest.java
+++ seleniumtestng/src/test/java/tests/LogInTest.java
@@ -1,2 +1,28 @@
-package tests;public class LogInTest {
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import pages.LogInPage;
+
+public class LogInTest {
+
+    @BeforeTest
+    void openBrowser() {
+        LogInPage.Setup();
+    }
+
+    @Test
+    void logInPageTest() {
+
+        LogInPage.ENTER_EMAIL();
+        LogInPage.ENTER_PASSWORD();
+        LogInPage.CLICK_SIGNIN_BUTTON();
+
+    }
+
+
+
+
 }
