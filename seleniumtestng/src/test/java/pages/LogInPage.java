4e4af3e3fa947be2a7406f675199fe1f6991179d
criscarlo.dapitanon <criscarlo.dapitanon@legalmatch.com>
2024-07-05 15:22:21 +0800

    Added headless chrome test script

--- seleniumtestng/src/test/java/pages/LogInPage.java
+++ seleniumtestng/src/test/java/pages/LogInPage.java
@@ -2,21 +2,43 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+
 
 public class LogInPage {
 
     public static WebDriver driver;
     public static void Setup() {
 
-        driver = new ChromeDriver();
+        // Setup ChromeDriver (ensure the chromedriver executable is in your PATH)
+        System.setProperty("chromedriver", "seleniumtestng/src/test/resources/drivers/chromedriver.exe");
+
+        // Set Chrome options to run in headless mode
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080");
+
+        driver = new ChromeDriver(options);
         driver.get("https://demo-store.katalon.com/signin");
 
     }
+
+    public static String logInPage_text_cssSelector = "#root > div > div > div > div > div > div.footer > div";
     public static String email_inputField_cssSelector = "input[name='email']";
 
     public static  String password_inputField_cssSelector = "input[name='password']";
     public  static  String signIn_button_cssSelector = "input[value='Sign In']";
+
+    public static void VALIDATE_SIGN_IN_PAGE_TEXT() {
+        WebElement element = driver.findElement(By.cssSelector(logInPage_text_cssSelector));
+        String actualText = element.getText();
+        String expectedText = "Don't you have an account? Sign Up";
+        Assert.assertEquals(actualText, expectedText, "Text does not match!");
+    }
 
     public static void ENTER_EMAIL() {
         driver.findElement(By.cssSelector(email_inputField_cssSelector)).sendKeys("crisdemo@email.com");
--- seleniumtestng/src/test/java/tests/LogInTest.java
+++ seleniumtestng/src/test/java/tests/LogInTest.java
@@ -1,16 +1,35 @@
 package tests;
 
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import pages.LogInPage;
 
+import static pages.LogInPage.driver;
+
 public class LogInTest {
 
+    @BeforeTest
+    void openBrowserPageTest() {
+        LogInPage.Setup();
+    }
 
-@Test
-    void logInTest() {
+    @Test
+    void validateSignInPageTextTest() {
+        LogInPage.VALIDATE_SIGN_IN_PAGE_TEXT();
+    }
 
-    LogInPage.ENTER_EMAIL();
-}
+    @Test
+    void enterEmailPasswordTest() {
+        LogInPage.ENTER_EMAIL();
+        LogInPage.ENTER_PASSWORD();
+    }
 
+    @AfterTest
+    public void tearDownTest() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 
 }
